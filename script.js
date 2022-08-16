03ed41d817ad7aa5a9851b8767c1d85687628baa
Pixz <tinycactusking@gmail.com>
2022-08-15 21:45:44 -0400

    Add operator function

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ function divide (n1, n2) {
     return n1 / n2;
 }
 
+function operate (operator, n1, n2) {
+    switch (operator) {
+        case 'add':
+            return add(n1,n2);
+
+        case 'subtract':
+            return subtract(n1,n2);
+
+        case 'multiply':
+            return multiply(n1,n2);
+        
+        case 'divide':
+            return divide(n1,n2);
+        
+        default:
+            console.log('ERROR: operator not found');
+    }
+}
+
